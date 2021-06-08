1ae539c4872f69afcbf815547b209d19927b1c42
Nitishkumar05 <85563379+Nitishkumar05@users.noreply.github.com>
2021-06-08 19:10:16 +0530

    Created hello.java
    This file prints hello world on the screen

---
+++ hello.java
@@ -0,0 +1,14 @@
+package javaapplication3;
+import java.util.*;
+
+public class JavaApplication3 {
+
+   
+    public static void main(String[] args)
+    {
+        System.out.println("Hello,world");
+        
+        
+    }
+    
+}
